2b077731a2241486305008381a608ab18d2e00a7
chinna0107 <kancharlahemanth89@gmail.com>
2026-06-28 12:41:20 +0530

    Production 2.23

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -59,6 +59,18 @@ const Header = () => {
     };
   }, []);
 
+  // Lock body scroll when mobile menu is open
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (
